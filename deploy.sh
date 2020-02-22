f3a9d91241009cc50aba47737ad0b4a835105674
naveedhuq <naveedhuq@gmail.com>
2020-02-22 01:18:46 -0500

    Completed GC Deployment

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t naveedhuq/multi-client:latest -t naveedhuq/multi-client:$SHA -f ./client/Docketfile ./client
+docker build -t naveedhuq/multi-server:latest -t naveedhuq/multi-server:$SHA -f ./server/Docketfile ./server
+docker build -t naveedhuq/multi-worker:latest -t naveedhuq/multi-worker:$SHA -f ./worker/Docketfile ./worker
+
+docker push naveedhuq/multi-client:latest
+docker push naveedhuq/multi-server:latest
+docker push naveedhuq/multi-worker:latest
+
+docker push naveedhuq/multi-client:$SHA
+docker push naveedhuq/multi-server:$SHA
+docker push naveedhuq/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployments server=naveedhuq/multi-server:$SHA
+kubectl set image deployments/client-deployments client=naveedhuq/multi-client:$SHA
+kubectl set image deployments/worker-deployments worker=naveedhuq/multi-worker:$SHA
